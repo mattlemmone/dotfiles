cf338578c002d3fa9b2193020b31a4d600defd7f
Matt Lemmone <matt.lemmone@doordash.com>
2022-09-06 22:54:59 +0900

    stylua

--- neovim/after/lua/options.lua
+++ neovim/after/lua/options.lua
@@ -1,66 +1,66 @@
-local set=vim.opt
-local g=vim.g
-local expand=vim.fn.expand
+local set = vim.opt
+local g = vim.g
+local expand = vim.fn.expand
 
-set.laststatus=2   -- always show it
+set.laststatus = 2 -- always show it
 
- -- Display
-set.cpoptions:append('>')      -- indicator for change motion
-set.cursorline=true        -- highlight current line
-set.encoding='utf-8'
-set.expandtab=true         -- convert <TAB> key-presses to spaces
-set.list=true              -- show special chars
-set.listchars={
-  eol = '↲',
-  tab = '› ',
-  trail = '·',
-  extends = '#',
-  nbsp = '.'
-}   -- special character overrides
- -- set.lcs+=space:•      -- dot instead of space
-set.number=true            -- show line numbers
-set.ruler=true             -- show cursor position
-set.showcmd=true
-set.textwidth=79
-set.tw=0              -- text width 0 wont allow linebreak
+-- Display
+set.cpoptions:append(">") -- indicator for change motion
+set.cursorline = true -- highlight current line
+set.encoding = "utf-8"
+set.expandtab = true -- convert <TAB> key-presses to spaces
+set.list = true -- show special chars
+set.listchars = {
+	eol = "↲",
+	tab = "› ",
+	trail = "·",
+	extends = "#",
+	nbsp = ".",
+} -- special character overrides
+-- set.lcs+=space:•      -- dot instead of space
+set.number = true -- show line numbers
+set.ruler = true -- show cursor position
+set.showcmd = true
+set.textwidth = 79
+set.tw = 0 -- text width 0 wont allow linebreak
 
- -- Indentation (Universal default)
-set.shiftwidth=2
-set.tabstop=2
+-- Indentation (Universal default)
+set.shiftwidth = 2
+set.tabstop = 2
 
- -- Search
-set.showmatch=true
-set.ignorecase=true
-set.smartcase=true      -- switch to case-sensitive searches when uppercase chars found
-set.incsearch=true      -- partial matches
-set.hlsearch=true       -- highlight matches
+-- Search
+set.showmatch = true
+set.ignorecase = true
+set.smartcase = true -- switch to case-sensitive searches when uppercase chars found
+set.incsearch = true -- partial matches
+set.hlsearch = true -- highlight matches
 
- -- Indentation (Universal default)
-set.shiftwidth=2
-set.tabstop=2
+-- Indentation (Universal default)
+set.shiftwidth = 2
+set.tabstop = 2
 
- -- Commands
-set.wildmenu=true       -- visual autocomplete
+-- Commands
+set.wildmenu = true -- visual autocomplete
 
 -- Python
-set.pyxversion=3
+set.pyxversion = 3
 
 -- neovim
-g.node_host_prog    = expand('~/.asdf/shims/node')
-g.python3_host_prog = expand('~/.pyenv/versions/neovim-python3/bin/python')
-g.python_host_prog  = expand('~/.pyenv/versions/neovim-python2/bin/python')
-g.ruby_host_prog    = expand('~/.rbenv/versions/2.6.1/bin/neovim-ruby-host')
+g.node_host_prog = expand("~/.asdf/shims/node")
+g.python3_host_prog = expand("~/.pyenv/versions/neovim-python3/bin/python")
+g.python_host_prog = expand("~/.pyenv/versions/neovim-python2/bin/python")
+g.ruby_host_prog = expand("~/.rbenv/versions/2.6.1/bin/neovim-ruby-host")
 
 -- vim-go
 g.go_highlight_build_constraints = 1
-g.go_highlight_functions         = 1
-g.go_highlight_interfaces        = 1
-g.go_highlight_methods           = 1
-g.go_highlight_operators         = 1
-g.go_highlight_structs           = 1
+g.go_highlight_functions = 1
+g.go_highlight_interfaces = 1
+g.go_highlight_methods = 1
+g.go_highlight_operators = 1
+g.go_highlight_structs = 1
 
 -- Plugins
-g.UltiSnipsExpandTrigger="<nop>" -- Let coc.nvim use tab instead of ultisnips
-g.gitgutter_map_keys=0           -- Don't use any key mappings
-g.NERDTreeQuitOnOpen=1               -- close browser after opening a new buffer
-g.BufKillCreateMappings=0
+g.UltiSnipsExpandTrigger = "<nop>" -- Let coc.nvim use tab instead of ultisnips
+g.gitgutter_map_keys = 0 -- Don't use any key mappings
+g.NERDTreeQuitOnOpen = 1 -- close browser after opening a new buffer
+g.BufKillCreateMappings = 0
