f30d65582825438a46fb42260635619a9f6f5f5e
Matt Lemmone <matt.lemmone@doordash.com>
2022-09-07 12:02:58 +0900

    zsh

---
+++ zsh/plugins.zsh
@@ -0,0 +1,8 @@
+# Plugins
+antibody bundle zdharma/fast-syntax-highlighting
+antibody bundle zsh-users/zsh-autosuggestions
+antibody bundle zsh-users/zsh-history-substring-search
+# antibody bundle zsh-users/zsh-completions
+antibody bundle mafredri/zsh-async
+antibody bundle sindresorhus/pure
+antibody bundle softmoth/zsh-vim-mode
