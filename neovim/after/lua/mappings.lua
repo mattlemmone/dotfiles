b64015992cabe40ae63b88ffe5dff7a0012db67a
Matt Lemmone <matt.lemmone@doordash.com>
2022-09-09 14:37:01 +0900

    fzf -> telescope

--- neovim/after/lua/mappings.lua
+++ neovim/after/lua/mappings.lua
@@ -21,30 +21,15 @@ keymap.set("t", "jk", "<C-\\><C-n>")
 keymap.set("n", "<Leader>O", "O<Esc>")
 keymap.set("n", "<Leader>o", "o<Esc>")
 
--- list open buffers
-keymap.set("n", "<Leader>b", ":Buffers<CR>")
+-- disable highlight
+keymap.set("n", "<Leader>hl", ":nohl<CR> ")
 
 -- reload current lua file
 keymap.set("n", "<Leader>l", ":luafile %<CR>")
 
--- command history
-keymap.set("n", "<Leader>ch", ":History:<CR>")
-
--- file search
-keymap.set("n", "<Leader>f", ":Files<CR>")
-keymap.set("n", "<Leader>g", ":Commits<CR>")
-
--- file history
-keymap.set("n", "<Leader>hh", ":History<CR>")
-
--- disable highlight
-keymap.set("n", "<Leader>hl", ":nohl<CR> ")
+-- reload vimrc
 keymap.set("n", "<Leader>r", ":so ~/dotfiles/neovim/.vimrc<CR>")
-keymap.set("n", "<Leader>r", ":so ~/dotfiles/neovim/.vimrc<CR>")
-keymap.set("n", "<Leader>s", ":Rg<CR>")
 
--- search for word under cursor in project
-keymap.set("n", "<Leader>*", ":Rg <C-R><C-W><CR> ")
 keymap.set("n", "<Leader>d", ":Bdelete<CR>")
 keymap.set("n", "<Leader>w", ":ArgWrap<CR>", { silent = true })
 
@@ -60,12 +45,6 @@ keymap.set("n", "<Leader>yf", ':let @+ = expand("%")<CR>', { silent = true })
 
 --" Rename instances
 keymap.set("n", "<Leader>?", ":Cheatsheet<CR>")
-
---" Goto
-keymap.set("n", "gd", "<Plug>(coc-definition)", { silent = true })
-keymap.set("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
-keymap.set("n", "gi", "<Plug>(coc-implementation)", { silent = true })
-keymap.set("n", "gr", "<Plug>(coc-references)", { silent = true })
 
 -- Split Navigation
 keymap.set("n", "<Leader>-", ":split<CR>")
---
+++ neovim/after/lua/plugins/config/fzf.lua
@@ -0,0 +1,46 @@
+local keymap = vim.keymap
+
+require("telescope").setup({
+	defaults = {
+  sorting_strategy = "ascending",
+		layout_config = {
+			vertical = { width = 0.25 },
+			prompt_position = "top",
+			-- other layout configuration here
+		},
+		-- other defaults configuration here
+	},
+	pickers = {
+		find_files = {
+			find_command = { "fd" },
+		},
+		live_grep = {
+			find_command = { "rcatg", "-l" },
+		},
+	},
+})
+
+-- list open buffers
+keymap.set("n", "<Leader>b", "<CMD>lua require('telescope.builtin').buffers()<CR>")
+
+-- search current buffer
+keymap.set("n", "<Leader>ll", "<CMD>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>")
+
+-- registers
+keymap.set("n", '<Leader>\"', "<CMD>lua require('telescope.builtin').registers()<CR>")
+
+-- command history
+keymap.set("n", "<Leader>ch", "<CMD>lua require('telescope.builtin').command_history()<CR>")
+
+-- file search
+keymap.set("n", "<Leader>f", "<CMD>lua require('telescope.builtin').find_files()<CR>")
+keymap.set("n", "<Leader>g", "<CMD>lua require('telescope.builtin').git_commits()<CR>")
+
+-- file history
+keymap.set("n", "<Leader>hh", "<CMD>lua require('telescope.builtin').oldfiles()<CR>")
+
+-- grep
+keymap.set("n", "<Leader>s", "<CMD>lua require('telescope.builtin').live_grep()<CR>")
+
+-- search for word under cursor in project
+keymap.set("n", "<Leader>*", ":Rg <C-R>:<C-W><CR> ")
