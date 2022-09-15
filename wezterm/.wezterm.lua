8a1f8b293cbbfa2d2bacfd179e083e3988564112
Matt Lemmone <matt.lemmone@doordash.com>
2022-09-15 14:54:51 +0900

    wezterm, neovim node js host fix

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,15 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
+wezterm.log_info("Config file " .. wezterm.config_file)
 
 return {
-  font = wezterm.font("FiraCode Nerd Font Mono", {font_size = 16} ),
-  color_scheme = "Catppuccin Mocha"
+	font = wezterm.font("FiraCode Nerd Font Mono", { font_size = 16 }),
+	color_scheme = "Catppuccin Mocha",
+	unix_domains = {
+		{
+			name = "unix",
+		},
+	},
+
+	default_gui_startup_args = { "connect", "unix" },
+	show_update_window = false,
 }
