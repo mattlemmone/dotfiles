0cd9634ec5721b6ab9fa2676ab565b95105e44c6
Matt Lemmone <matt.lemmone@wolt.com>
2023-02-09 20:18:41 +0900

    who can really say what im doing anymore

--- neovim/after/lua/lmnlym/autocmd.lua
+++ neovim/after/lua/lmnlym/autocmd.lua
@@ -2,52 +2,61 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- Format on save
 autocmd("BufWritePre", {
-  pattern = {
-    "*.lua",
-    "*.html",
-    "*.json",
-  },
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+	pattern = {
+		"*.lua",
+		"*.html",
+		"*.json",
+	},
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
 
--- Format js/ts on save
 autocmd("BufWritePre", {
-  pattern = {
-    "*.js",
-    "*.ts",
-  },
-  callback = function()
-    vim.cmd("EslintFixAll")
-  end,
+	pattern = {
+		"*.ts",
+		"*.tsx",
+		"*.js",
+		"*.jsx",
+	},
+	callback = function()
+		-- On save, only use null-ls
+		vim.lsp.buf.format({
+			-- bufnr = vim.api.nvim_get_current_buf(),
+			-- filter = function(client)
+			-- 	return client.name == "null-ls"
+			-- end,
+			async = false,
+			timeout_ms = 2000,
+		})
+	end,
 })
 
 -- Highlight after yank
 autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ timeout = 600 })
-  end,
-})
-
--- Enable relative numbers
-autocmd("InsertEnter", {
-  callback = function()
-    vim.opt.relativenumber = true
-  end,
+	callback = function()
+		vim.highlight.on_yank({ timeout = 600 })
+	end,
 })
 
 -- Disable relative numbers
+autocmd("InsertEnter", {
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
+
+-- Enable relative numbers
 autocmd("InsertLeave", {
-  callback = function()
-    vim.opt.relativenumber = false
-  end,
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
 })
 
 -- Enable spellchecking in markdown, text and gitcommit files
 autocmd("FileType", {
-  pattern = { "gitcommit", "markdown", "text", "tex" },
-  callback = function()
-    vim.opt_local.spell = true
-  end,
+	pattern = { "gitcommit", "markdown", "text", "tex" },
+	callback = function()
+		vim.opt_local.spell = true
+	end,
 })
--- neovim/after/lua/lmnlym/plugins/config/main.lua
+++ neovim/after/lua/lmnlym/plugins/config/main.lua
@@ -3,7 +3,7 @@ require("impatient") -- keep this at the top
 require("leap").add_default_mappings()
 require("nvim-autopairs").setup({})
 require("pretty-fold").setup({})
-require("treesitter-context").setup({})
+-- require("treesitter-context").setup({})
 require("twilight").setup({})
 require("which-key").setup({})
 
@@ -19,39 +19,39 @@ require("lmnlym.plugins.config.tundra")
 require("lmnlym.plugins.config.vim-plug")
 
 require("indent_blankline").setup({
-  show_current_context = true,
-  show_current_context_start = false,
+	show_current_context = true,
+	show_current_context_start = false,
 })
 
 require("nvim-treesitter.configs").setup({
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
-  highlight = { enable = true },
-  indent = { enable = true },
+	-- Automatically install missing parsers when entering buffer
+	auto_install = true,
+	highlight = { enable = true },
+	indent = { enable = true },
 })
 
 require("gitsigns").setup({
-  current_line_blame = true,
+	current_line_blame = true,
 })
 
 require("bufferline").setup({
-  highlights = {
-    buffer_selected = { italic = false },
-    diagnostic_selected = { italic = false },
-    hint_selected = { italic = false },
-    pick_selected = { italic = false },
-    pick_visible = { italic = false },
-    pick = { italic = false },
-  },
+	highlights = {
+		buffer_selected = { italic = false },
+		diagnostic_selected = { italic = false },
+		hint_selected = { italic = false },
+		pick_selected = { italic = false },
+		pick_visible = { italic = false },
+		pick = { italic = false },
+	},
 })
 
 require("fidget").setup({
-  text = { spinner = "dots" },
-  align = { bottom = false },
-  window = {
-    relative = "win", -- where to anchor, either "win" or "editor"
-    blend = 100, -- &winblend for the window
-    zindex = 1, -- the zindex value for the window
-    border = "rounded", -- style of border for the fidget window
-  },
+	text = { spinner = "dots" },
+	align = { bottom = false },
+	window = {
+		relative = "win", -- where to anchor, either "win" or "editor"
+		blend = 100, -- &winblend for the window
+		zindex = 1, -- the zindex value for the window
+		border = "rounded", -- style of border for the fidget window
+	},
 })
