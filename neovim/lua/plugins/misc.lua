2fcc37b941ba78f135b639c0689afc954329cb91
mattlemmone <mattlemmone@gmail.com>
2023-08-04 20:05:25 +0900

    remove some thangs

--- neovim/lua/plugins/misc.lua
+++ neovim/lua/plugins/misc.lua
@@ -2,7 +2,6 @@ return {
   "MunifTanjim/nui.nvim",                                     -- ui
   { "nvim-lua/plenary.nvim",            lazy = true },        -- misc lua functions
   { "kristijanhusak/vim-carbon-now-sh", event = "VeryLazy" }, -- Screenshot and upload code to carbon.now.sh
-  { "metakirby5/codi.vim",              event = "VeryLazy" }, -- live eval mode
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -10,16 +9,5 @@ return {
       require("which-key").setup({})
     end,
   },
-  {
-    "michaelb/sniprun",
-    event = "VeryLazy",
-    build = "bash install.sh",
-    config = function()
-      require("sniprun").setup({
-        display = {
-          "NvimNotify", --# display with the nvim-notify plugin
-        },
-      })
-    end,
-  }, -- execute lines of selected code
+  -- execute lines of selected code
 }
