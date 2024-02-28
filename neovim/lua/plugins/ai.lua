053e32ce654143ace2854bdc9f37cc20e4166a24
Matt Lemmone <github@mattlemmone.com>
2024-02-28 09:41:54 +0900

    custom ai cmd

--- neovim/lua/plugins/ai.lua
+++ neovim/lua/plugins/ai.lua
@@ -7,6 +7,23 @@ return {
     },
     config = function()
       require("codegpt.config")
+      vim.g["codegpt_commands"] = {
+        ["testwith"] = {
+          user_message_template =
+              "Write tests for the following code: ```{{filetype}}\n{{text_selection}}```\n{{command_args}} " ..
+              "Only return the code snippet and nothing else.",
+          callback_type = "code_popup",
+          model = "gpt-4"
+        }
+      }
+
+      vim.g["codegpt_commands"] = {
+        ["completion"] = { model = "gpt-4" },
+        ["code_edit"] = { model = "gpt-4" },
+        ["opt"] = { model = "gpt-4" },
+        ["tests"] = { model = "gpt-4" },
+        ["explain"] = { model = "gpt-4" },
+      }
     end
   },
   {
