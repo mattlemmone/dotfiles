4e638b6c881d5a6582af203cc6cf3cd2b239f476
Matt Lemmone <github@mattlemmone.com>
2024-04-01 12:40:18 +0900

    merge lsp

---
+++ neovim/lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"j-hui/fidget.nvim",
+		event = "VeryLazy",
+		tag = "legacy",
+		config = function()
+			require("fidget").setup({ text = { spinner = "dots" } })
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig", -- easier lsp mgmt
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{
+				"yioneko/nvim-vtsls",
+				ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+				event = { "BufReadPre", "BufNewFile" },
+			},
+		},
+		config = function()
+			require("mlem.lsp.init").setup()
+		end,
+	},
+}
--- neovim/lua/plugins/lsp_loading_status.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"j-hui/fidget.nvim",
-	event = "VeryLazy",
-	tag = "legacy",
-	config = function()
-		require("fidget").setup({ text = { spinner = "dots" } })
-	end,
-}
