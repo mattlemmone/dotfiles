7421ce419713c2e2ad6f995e704979c4af130022
mattlemmone <mattlemmone@gmail.com>
2024-11-24 00:09:10 +0900

    ded kode

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -22,7 +22,6 @@ return {
 		})
 
 		local autocmd = vim.api.nvim_create_autocmd
-		local command = vim.api.nvim_create_user_command
 
 		autocmd("BufWritePre", {
 			pattern = { "*" },
@@ -39,21 +38,5 @@ return {
 				-- vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" } } })
 			end,
 		})
-
-		command("Format", function(args)
-			local range = nil
-			if args.count ~= -1 then
-				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-				range = {
-					start = { args.line1, 0 },
-					["end"] = { args.line2, end_line:len() },
-				}
-			end
-			require("conform").format({
-				async = true,
-				lsp_fallback = "always",
-				range = range,
-			})
-		end, { range = true })
 	end,
 }
