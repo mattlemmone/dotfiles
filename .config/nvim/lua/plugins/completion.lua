c390338a90398d3e9ef615b425f0e6908adf26d0
Matt Lemmone <github@mattlemmone.com>
2024-12-20 11:01:41 +0900

    meh

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -104,8 +104,9 @@ return {
 					{ name = "luasnip", group_index = 1 },
 					{ name = "nvim_lsp", group_index = 2 },
 					{ name = "copilot", group_index = 2 },
-					{ name = "buffer", group_index = 2 },
-					{ name = "path", group_index = 2 },
+					{ name = "nvim_lua", group_index = 2 },
+					{ name = "buffer", group_index = 3, min_length = 3 },
+					{ name = "path", group_index = 3 },
 				}),
 			})
 
@@ -114,7 +115,6 @@ return {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
 					{ name = "nvim_lsp_document_symbol" },
-					-- { name = "buffer" },
 				},
 			})
 
